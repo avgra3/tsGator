f2e1c2b437f04098697144fb589e4d4c614eac0e
avgra3 <antonygradillas@gmail.com>
2025-10-07 15:23:42 -0700

    Added License

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -6,16 +6,12 @@ import { users } from "../schema";
 // USER TABLE FUNCTIONS
 export async function createUser(name: string) {
 	const userCheck = await getUserByName(name);
-	// userCheck.forEach((value, index) => {
-	// 	console.log(`- index[${index}] => value: ${value}`);
-	// });
 	if (userCheck.length > 0) {
 		throw new Error(`ERROR: User "${name}" already exists!`);
 	}
 	const [result] = await db.insert(users)
 		.values({ name: name })
 		.returning();
-	// console.log(`RESULT => ${result}`)
 	return result;
 }
 
@@ -23,7 +19,6 @@ export async function getUserByName(name: string) {
 	const result = await db.select()
 		.from(users)
 		.where(eq(users.name, name));
-	// console.log(`Currently ${result.length} users with name="${name}"!`);
 	return result;
 }
 
@@ -36,7 +31,6 @@ export async function getUsersByIDs(ids: string[]) {
 	const result = await db.select().from(users).where(inArray(users.id, ids));
 	return result
 }
-//inArray(table.column, query)
 
 export async function getAllUsers() {
 	const result = await db.select()
